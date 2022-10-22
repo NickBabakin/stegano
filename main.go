c76a216c20a9fc2228200e13941a2d2ab3a5e5d2
NickBBn <nickgeo.winner@gmail.com>
2022-10-22 15:41:45 +0300

    Simple file work added

--- main.go
+++ main.go
@@ -3,25 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
-
-	"github.com/NickBabakin/stegano/standartstegano"
 )
 
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
-	container := []byte("Hello, World! This is my first day here. I am so glad to see all of you")
-	data := []byte("Love")
-	fmt.Printf("\nMessage: \n - %s\n", data)
-	fmt.Printf("Container: \n - %s\n", container)
-	bytesOfSize := 2
-	err := standartstegano.PerformStandartEncryption(container, data, bytesOfSize)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Container with hidden message: \n - %s\n", container)
-	fmt.Printf("\n--------------------------------\n")
-	decryptedInfo, err := standartstegano.PerformStandartDecryption(container, bytesOfSize)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Extracted from container message: \n - %s\n\n", decryptedInfo)
+	fmt.Println("Hello World!")
 }
---
+++ main_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"github.com/NickBabakin/stegano/standartstegano"
+)
+
+func testErr(err error, t *testing.T) {
+	if err != nil {
+		t.Errorf("Error: %s", err.Error())
+	}
+}
+
+func TestFullWithText(t *testing.T) {
+	container := []byte("Hello, World! This is my first day here. I am so glad to see all of you")
+	message := []byte("Love")
+	fmt.Printf("\nMessage: \n - %s\n", message)
+	fmt.Printf("Container: \n - %s\n", container)
+	bytesOfSize := 2
+	err := standartstegano.PerformStandartEncryption(container, message, bytesOfSize)
+	testErr(err, t)
+	fmt.Printf("Container with hidden message: \n - %s\n", container)
+
+	fmt.Printf("\n--------------------------------\n")
+
+	decryptedMessage, err := standartstegano.PerformStandartDecryption(container, bytesOfSize)
+	testErr(err, t)
+	fmt.Printf("Extracted from container message: \n - %s\n\n", decryptedMessage)
+	if string(decryptedMessage) != string(message) {
+		t.Errorf("\nexpected: %s\ngot: %s", decryptedMessage, message)
+	}
+}
+
+func TestFullBmp(t *testing.T) {
+
+	fileData, err := ioutil.ReadFile("abc.bmp")
+	container := fileData[100:]
+	testErr(err, t)
+
+	message := []byte("Love")
+	fmt.Printf("\nMessage: \n - %s\n", message)
+	fmt.Printf("Container: \n - %s\n", container[:100])
+	bytesOfSize := 2
+	err = standartstegano.PerformStandartEncryption(container, message, bytesOfSize)
+	testErr(err, t)
+	fmt.Printf("Container with hidden message: \n - %s\n", container[:100])
+	err = ioutil.WriteFile("file.bmp", fileData, 0600)
+	testErr(err, t)
+
+	fmt.Printf("\n--------------------------------\n")
+
+	decryptedMessage, err := standartstegano.PerformStandartDecryption(container, bytesOfSize)
+	testErr(err, t)
+	fmt.Printf("Extracted from container message: \n - %s\n\n", decryptedMessage)
+	if string(decryptedMessage) != string(message) {
+		t.Errorf("\nexpected: %s\ngot: %s", decryptedMessage, message)
+	}
+}
